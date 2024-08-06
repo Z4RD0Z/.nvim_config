5a84535ab2832c1ff55d356ae7a630fdfad89b18
Andrea Lomazzo <58508926+Z4RD0Z@users.noreply.github.com>
2024-08-06 15:09:16 +0200

    Update init.lua

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,25 +1,60 @@
-return {{
+return {
+  {
     "stevearc/conform.nvim",
     event = "BufWritePre",
     cmd = "ConformInfo",
     config = function()
-        require "configs.conform"
-    end
-}, -- These are some examples, uncomment them if you want to see them work!
-{
+      require("configs.conform")
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
-        require("nvchad.configs.lspconfig").defaults()
-        require "configs.lspconfig"
-    end
-}, {
+      require("nvchad.configs.lspconfig").defaults()
+      require("configs.lspconfig")
+    end,
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
-        ensure_installed = {"lua-language-server", "stylua", "html-lsp", "css-lsp", "prettier"}
-    }
-}, {
+      ensure_installed = { "lua-language-server", "stylua", "html-lsp", "css-lsp", "prettier" },
+    },
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-        ensure_installed = {"vim", "lua", "vimdoc", "html", "css"}
-    }
-}}
+      ensure_installed = { "vim", "lua", "vimdoc", "html", "css" },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      require("configs.dap")
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    config = function()
+      require("dapui").setup()
+    end,
+    requires = { "mfussenegger/nvim-dap" },
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    config = function()
+      require("nvim-dap-virtual-text").setup()
+    end,
+    requires = { "mfussenegger/nvim-dap" },
+  },
+  {
+    "nvim-neotest/nvim-nio",
+  },
+  {
+    "mfussenegger/nvim-lint",
+    lazy=false,
+    event = { "BufReadPre", "BufNewFile", "SessionLoadPost" },
+    config = function()
+      require("configs.nvim-lint")
+    end,
+  },
+}
